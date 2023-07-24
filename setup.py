34559c57c4f37d708026e5e8627a7286574b831b
Ashkan Goleh poor <39386474+ashkangoleh@users.noreply.github.com>
2023-07-24 11:45:41 +0330

    modified:   setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='fastapi_dynamic_routers',
-    version='0.1',
+    version='0.1.1',
     packages=find_packages(),
     install_requires=[
         'fastapi',
